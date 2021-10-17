a06594c06fc794141518917a9b2fb45584f89699
Pawan Bharti <bpawan96@gmail.com>
2021-10-18 01:55:36 +0530

    Low Level Caching

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 # There was issue in seed.rb
 * uninitialized constant Faker::StarWars
+
+# Low Level Caching
+
+The first type of caching I'd like to discuss is called low level or model caching. In my opinion, this is the simplest type of caching that still can (when used properly) boost the performance of some particular page quite significantly. The idea is that we cache the results of a complex query and return them without re-running the same query over and over again.
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,28 @@
+# Employee Model
+
 class Employee < ApplicationRecord
   VALID_GENDER = %w(male female)
+  after_commit :flush_cache!
 
   scope :by_gender, ->(gender) do
     if VALID_GENDER.include?(gender)
-      where gender: gender
+      # where gender: gender
+      Rails.cache.fetch("employees_#{gender}") { puts 'evaluating...'; where gender: gender }
     else
-      all
+      # all
+      Rails.cache.fetch('all_employees') { puts 'evaluating...'; all }
     end
+  end
+
+  def final_salary
+    Rails.cache.fetch("#{cache_key}/tax") { puts 'calculating tax...'; salary - salary * 0.13 }
+  end
+
+  private
+
+  def flush_cache!
+    puts 'flushing the cache...'
+    Rails.cache.delete 'all_employees'
+    Rails.cache.delete "employees_#{gender}"
   end
 end
